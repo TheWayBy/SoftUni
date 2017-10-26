aab3318588b870341d14746cad6158d7670cae32
VVeleva <viktoriqvveleva@gmail.com>
2017-10-26 12:39:43 +0300

    Drawing Figures Part 2

--- 06.DrawingFigureswithLoops/08.House/Program.cs
+++ 06.DrawingFigureswithLoops/08.House/Program.cs
@@ -10,6 +10,24 @@ namespace _08.House
     {
         static void Main(string[] args)
         {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= (num + 1) / 2; i++)
+            {
+                if (num % 2 == 0)
+                {
+                    Console.Write(new string('-', (num / 2) - i));
+                    Console.Write(new string('*', i * 2));
+                    Console.Write(new string('-', (num / 2) - i));
+                }
+                else
+                {
+                    Console.Write(new string('-', (num /2) - i - 1));
+                    Console.Write(new string('*', i == 1 ? i : i*2 - 1));
+                    Console.Write(new string('-', (num / 2) - i - 1));
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
