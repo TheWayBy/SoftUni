219e73319b640b1bf07d1318c062d5eb56048e8a
VVeleva <viktoriqvveleva@gmail.com>
2017-10-29 18:07:36 +0200

    Programming Basics Exam - 17 July 2016 Part 2

---
+++ ProgrammingBasicsExam 17July2016/05.Diamond/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Diamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= num; i++)
+            {
+                if (i == 0 || i == num)
+                {
+                    Console.Write(new string('.', num - i));
+                    Console.Write(new string('*', (5 * num) - 2 * (num - i)));
+                    Console.WriteLine(new string('.', num - i));
+                }
+                else
+                {
+                    Console.Write(new string('.', num - i));
+                    Console.Write('*');
+                    Console.Write(new string('.', (5 * num) - 2 * (num - i) - 2));
+                    Console.Write('*');
+                    Console.WriteLine(new string('.', num - i));
+                }
+            }
+            for (int i = 1; i <= 2*num + 1; i++)
+            {
+                if (i == 2 * num + 1)
+                {
+                    Console.Write(new string('.', i));
+                    Console.Write(new string('*', 5 * num - 2 * i));
+                    Console.WriteLine(new string('.', i));
+
+                }
+                else
+                {
+                    Console.Write(new string('.', i));
+                    Console.Write('*');
+                    Console.Write(new string('.', (5 * num) - 2 * i - 2));
+                    Console.Write('*');
+                    Console.WriteLine(new string('.', i));
+                }
+               
+            }
+        }
+    }
+}
---
+++ ProgrammingBasicsExam 17July2016/06.StopNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.StopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int S = int.Parse(Console.ReadLine());
+
+            for (int i = M; i >= N; i--)
+            {
+                // Console.WriteLine(i);
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    if (i == S)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("{0} ", i);
+                    }
+                }
+            }
+        }
+    }
+}
